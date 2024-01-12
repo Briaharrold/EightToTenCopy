8a19219b2a8bb1d4641eddcc3fef10dc194b3be3
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-01-11 22:52:23 -0800

    Guessit Done

--- Controllers/Minich8Controller.cs
+++ Controllers/Minich8Controller.cs
@@ -20,33 +20,19 @@ namespace EightToTenCopy.Controllers;
         return _minich8Service.GuessItEasy(guess);
     } 
     
-//        private readonly IMinich8Service _minich8Service;
-//     public Minich8Controller(IMinich8Service minich8Service)
-//     {
-//         _minich8Service = minich8Service;
 
-//     }
-//     [HttpGet]
-//     [Route("GuessIt/Easy/{guess}")]
-//     public string GuessItEasy(string guess)
-//     {
-//         return _minich8Service.GuessItEasy(guess);
-//     } 
-    
+    [HttpGet]
+    [Route("GuessIt/Medium/{guess}")]
+    public string GuessItMedium(string guess)
+    {
+         return _minich8Service.GuessItMedium(guess);
+    }
 
-//     [HttpGet]
-//     [Route("GuessIt/Medium/{guess}")]
-//     public string GuessItMedium(string guess)
-//     {
-//          return _minich8Service.GuessItMedium(guess);
-//     }
+    [HttpGet]
+    [Route("GuessIt/Hard/{guess}")]
+    public string GuessItHard(string guess)
+    {
+        return _minich8Service.GuessItHard(guess);
+    }
+  }
 
-//     [HttpGet]
-//     [Route("GuessIt/Easy/{guess}")]
-//     public string GuessItHard(string guess)
-//     {
-//         return _minich8Service.GuessItHard(guess);
-//     }
-//   }
-
-}
